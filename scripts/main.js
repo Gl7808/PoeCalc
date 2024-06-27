15a275f7094c315644e15749bdbc359e0a61dfb3
Gl7808 <whorshunter42@yandex.ru>
2024-06-27 22:15:03 +0300

    Farm calculator start

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+
+
+let FarmCounter = [
+    {
+        divineOrbPrice : 0,
+        divineOrbAmount : 0,
+        divineOrbExchange : 0,
+        funPayTax : 0,
+        logBookAmount : 0,
+        day : 1,
+        time : 0,
+    },
+    {
+        divineOrbPrice : 0,
+        divineOrbAmount : 0,
+        divineOrbExchange : 0,
+        funPayTax : 0,
+        logBookAmount : 0,
+        day : 2,
+        time : 0,
+    },
+    {
+        divineOrbPrice : 0,
+        divineOrbAmount : 0,
+        divineOrbExchange : 0,
+        funPayTax : 0,
+        logBookAmount : 0,
+        day : 3,
+        time : 0,
+    },
+]
