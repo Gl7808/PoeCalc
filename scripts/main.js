913b4899cc0a2cbab82d4693f792e81f081d0ff4
Gl7808 <whorshunter42@yandex.ru>
2024-07-21 20:18:07 +0300

    links update

--- scripts/main.js
+++ scripts/main.js
@@ -2,30 +2,11 @@
 
 let FarmCounter = [
     {
-        divineOrbPrice : 0,
-        divineOrbAmount : 0,
-        divineOrbExchange : 0,
+        divineOrbPrice : 16,
+        divineOrbAmount : 56,
+        realProfit : 0,
         funPayTax : 0,
-        logBookAmount : 0,
         day : 1,
-        time : 0,
-    },
-    {
-        divineOrbPrice : 0,
-        divineOrbAmount : 0,
-        divineOrbExchange : 0,
-        funPayTax : 0,
-        logBookAmount : 0,
-        day : 2,
-        time : 0,
-    },
-    {
-        divineOrbPrice : 0,
-        divineOrbAmount : 0,
-        divineOrbExchange : 0,
-        funPayTax : 0,
-        logBookAmount : 0,
-        day : 3,
-        time : 0,
     },
 ]
+
